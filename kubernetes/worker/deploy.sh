30254c647798a35f272a65beedec4d8c8adf6c16
Eberhard Hummel <eberhard@hummel.ca>
2021-06-02 15:31:51 -0700

    Update deploy.sh

--- kubernetes/worker/deploy.sh
+++ kubernetes/worker/deploy.sh
@@ -25,6 +25,14 @@ function buildup {
   sudo apt-get update
   sudo apt-get install -y docker-ce docker-ce-cli containerd.io
   apt-cache madison docker-ce
+  sudo cp daemon.json /etc/docker/daemon.json
+  #sudo systemctl restart docker
+  echo "sleeping for 60 seconds to let docker finish installing"
+  sleep 60
+  echo "stopping docker service"
+  systemctl stop docker
+  echo "starting docker service"
+  systemctl start docker
   sudo apt-get update
   sudo apt-get install -y containerd.io
   sudo service docker start
@@ -36,12 +44,14 @@ function buildup {
   sudo apt-get update && sudo apt-get install -y kubelet kubeadm kubectl
   sudo systemctl enable kubelet
   sudo systemctl start kubelet
-  kubeadm join master:6443 --token 54imxs.qj3cjf5wdz0c5xnr 	--discovery-token-ca-cert-hash sha256:0e4dd6334ed96c27ac72a7320b4f06dbaa7f780fe01ac4c50e34044a6c735d61
-  kubectl delete node kube-slave
-  kubectl drain kube-master --ignore-daemonsets --delete-local-data
-  kubectl drain kube-slave --ignore-daemonsets --delete-local-data
-  kubectl drain kube-worker --ignore-daemonsets --delete-local-data
-  kubectl cluster info
+  
+  #grep the following command from the master node deploy script
+  #kubeadm join master:6443 --token oefhp7.js7ggsgvm1c8sadr 	--discovery-token-ca-cert-hash sha256:cde6e1350c21510b712699d4fd4c73bfbc77da8d120694fbc8bbe8a1d34790be 
+  sshpass -p $(cat .password) scp root@master:/root/kubeadmin-init.log .
+  tail -n 2 kubeadmin-init.log > kubeadmin-worker-join.sh
+  chmod a+x kubeadmin-worker-join.sh
+  ./kubeadmin-worker-join.sh
+  #kubeadm join master:6443 --token 54imxs.qj3cjf5wdz0c5xnr 	--discovery-token-ca-cert-hash sha256:0e4dd6334ed96c27ac72a7320b4f06dbaa7f780fe01ac4c50e34044a6c735d61
   kubectl cluster-info
   kubectl cluster-info dump
   sudo cp daemon.json /etc/docker/daemon.json
@@ -51,24 +61,23 @@ function buildup {
   sudo systemctl enable kubelet
   sudo systemctl start kubelet
 
-  #grep the following command from the master node deploy script
-  #kubeadm join master:6443 --token oefhp7.js7ggsgvm1c8sadr 	--discovery-token-ca-cert-hash sha256:cde6e1350c21510b712699d4fd4c73bfbc77da8d120694fbc8bbe8a1d34790be 
-  sshpass -p $(cat .password) scp root@master:/root/kubeadmin-init.log .
-  tail -n 2 kubeadmin-init.log > kubeadmin-worker-join.sh
-  chmod a+x kubeadmin-worker-join.sh
-  ./kubeadmin-worker-join.sh
+
 }
 
 #teardown
 function teardown {
   echo "start tear down"
-  kubectl delete clusterrolebinding kubernetes-dashboard-anonymous
-  kubectl drain kube-master --ignore-daemonsets --delete-emptydir-data
+  kubectl drain kube-slave --ignore-daemonsets --delete-emptydir-data
+  kubectl delete node kube-slave
+  kubectl drain kube-worker --ignore-daemonsets --delete-local-data
+  kubectl delete node kube-worker
+  
+  kubectl cluster info
   systemctl stop kubelet
-  kubectl delete node kube-master
   kubectl delete node kube-slave
   kubectl -n kubernetes-dashboard delete pod,svc --all
   kubectl -n kube-system delete pod,svc --all
+  
   sudo apt-get -y remove kubelet
   sudo apt-get -y remove kubernetes-cni
   sudo apt-get -y remove kubectl
@@ -76,7 +85,6 @@ function teardown {
   sudo apt-get -y remove kubeadm
   sudo apt-get -y remove docker-ce
   #sudo apt-get -y remove golang
-  #sudo apt-get -y remove libvirt
   sudo apt -y autoremove
   sudo rm -rf /etc/kubernetes/*
   sudo rm -rf /var/lib/docker/*
