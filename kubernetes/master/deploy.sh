0477e3570a2fd5c6ae45b7b39973fc23ce31425b
Eberhard Hummel <eberhard@hummel.ca>
2021-06-03 15:43:57 -0700

    Update deploy.sh

--- kubernetes/master/deploy.sh
+++ kubernetes/master/deploy.sh
@@ -12,7 +12,6 @@ function buildup {
   #sudo apt-get update
   #sudo apt-get install -y golang
   echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-  sudo apt-get update
   sudo apt-key adv --keyserver hkp://pool.sks-keyservers.net:80 --recv-keys 7EA0A9C3F273FCD8
   sudo apt-get update
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor --yes -o /usr/share/keyrings/docker-archive-keyring.gpg
