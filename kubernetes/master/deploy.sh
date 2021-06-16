5f42fce8001a80370950919a0e0f6b2a2991f014
Eberhard Hummel <eberhard@hummel.ca>
2021-06-16 16:31:31 -0700

    Update deploy.sh

--- kubernetes/master/deploy.sh
+++ kubernetes/master/deploy.sh
@@ -17,7 +17,7 @@ function buildup {
   #curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor --yes -o /usr/share/keyrings/docker-archive-keyring.gpg
   #echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu hirsute stable"
+  sudo add-apt-repository -y "deb [arch=amd64] https://download.docker.com/linux/ubuntu hirsute stable"
   sudo apt-cache madison docker-ce
   echo "running sudo apt-get install -y docker-ce"
   sudo apt-get install -y docker-ce 
